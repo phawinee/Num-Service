39a2fea3e3c20204671f498155e76b7d4da34195
phawinee.tak <phawinee.tak@gmamil.com>
2016-08-06 13:22:45 +0700

    Check EditText finish

---
+++ app/src/main/java/com/dachmontree/numservice/MyAlert.java
@@ -0,0 +1,40 @@
+package com.dachmontree.numservice;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by mywallet on 8/6/16 AD.
+ */
+public class MyAlert {
+    public void myDialog(Context context,
+                         int intAvata,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);///set undo ใช้ไม่ได้
+        builder.setIcon(findAvata(intAvata));
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+    public int findAvata(int intAvata) {
+        int[] resultInts = new int[] {R.drawable.bird48,R.drawable.doremon48,R.drawable.kon48,R.drawable.nobita48,R.drawable.rat48};
+
+
+
+
+        return resultInts[intAvata];
+    }
+
+
+}//main class
--- app/src/main/java/com/dachmontree/numservice/SignUpActivity.java
+++ app/src/main/java/com/dachmontree/numservice/SignUpActivity.java
@@ -2,12 +2,51 @@ package com.dachmontree.numservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+    //Explicit
+    private EditText nameEditText , userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton avata0RadioButton,avata1RadioButton,avata2RadioButton,avata3RadioButton,avata4RadioButton;
+    private String nameString,userString,passString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText);
+        radioGroup = (RadioGroup) findViewById(R.id.regAvata);
+        avata0RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avata1RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avata2RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avata3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avata4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+    }//Main method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passString = passwordEditText.getText().toString().trim();
+
+        //check
+        if (nameString.equals("")  || userString.equals("")||passString.equals("")) {
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, 0,"มีช่องว่าง","กรุณากรอกทุกช่อง");
+
+        }
+
+    }//click sign
+}//Main class
