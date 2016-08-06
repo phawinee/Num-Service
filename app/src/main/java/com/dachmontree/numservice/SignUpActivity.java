8a0524fa0bf7fbf18bedb524e470c66963c93900
phawinee.tak <phawinee.tak@gmamil.com>
2016-08-06 13:46:56 +0700

    Check radio finish

--- app/src/main/java/com/dachmontree/numservice/SignUpActivity.java
+++ app/src/main/java/com/dachmontree/numservice/SignUpActivity.java
@@ -12,7 +12,7 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText nameEditText , userEditText, passwordEditText;
     private RadioGroup radioGroup;
     private RadioButton avata0RadioButton,avata1RadioButton,avata2RadioButton,avata3RadioButton,avata4RadioButton;
-    private String nameString,userString,passString;
+    private String nameString,userString,passString,avataString;
 
 
     @Override
@@ -33,6 +33,29 @@ public class SignUpActivity extends AppCompatActivity {
         avata3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
         avata4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.radioButton:
+                        avataString = "0";
+                        break;
+                    case  R.id.radioButton2:
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avataString = "3";
+                        break;
+                    case R.id.radioButton5:
+                        avataString = "4";
+                        break;
+                }
+            }
+        });
+
     }//Main method
 
     public void clickSignUpSign(View view) {
@@ -45,6 +68,13 @@ public class SignUpActivity extends AppCompatActivity {
         if (nameString.equals("")  || userString.equals("")||passString.equals("")) {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, 0,"มีช่องว่าง","กรุณากรอกทุกช่อง");
+
+        }else if(avata0RadioButton.isChecked() || avata1RadioButton.isChecked() ||avata2RadioButton.isChecked()||avata3RadioButton.isChecked()||avata4RadioButton.isChecked()){
+
+        }else{
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, 1,"ยังไม่เลือก Avata","กรุณากรอกเลือก Avata ด้วยคร๊าบบบบ");
+
 
         }
 
