24c39d8010ffbddcee2cf4ea486d69c05febbeb8
phawinee.tak <phawinee.tak@gmamil.com>
2016-08-07 09:53:21 +0700

    get data finish

--- app/src/main/java/com/dachmontree/numservice/SignUpActivity.java
+++ app/src/main/java/com/dachmontree/numservice/SignUpActivity.java
@@ -9,9 +9,15 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
     //Explicit
@@ -118,7 +124,20 @@ public class SignUpActivity extends AppCompatActivity {
                 .add("User", userString)
                 .add("Password", passString)
                 .add("Avata", avataString).build();
+        Request.Builder builder =new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/6aug/add_user_noom.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
 
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
 
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
     }
 }//Main class
