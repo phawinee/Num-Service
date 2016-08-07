fea4c2869e0179594ab4cd323e303e66f2de9a65
phawinee.tak <phawinee.tak@gmamil.com>
2016-08-07 10:42:41 +0700

    check  finish

--- app/src/main/java/com/dachmontree/numservice/MainActivity.java
+++ app/src/main/java/com/dachmontree/numservice/MainActivity.java
@@ -4,17 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText userEditText,passEditText;
+    private String userString,passString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passEditText = (EditText) findViewById(R.id.editText5);
     }//main Method
+
+    public void clickSignInMain(View view){
+
+        userString = userEditText.getText().toString().trim();
+        passString = passEditText.getText().toString().trim();
+
+
+        if (userString.equals("") || passString.equals("")) {
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,3,"Have Space","please fill all ever black");
+        }else{
+
+        }
+
+    }
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
     }
+
+
 
 }// Main class
